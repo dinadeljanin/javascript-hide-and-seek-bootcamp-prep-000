6ddebdf342404439316ca165b7616a1a7ed53e7a
D D <ddeljanin5@icloud.com>
2020-03-09 19:00:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+function getFirstSelector(selector) {
+ return document.querySelector(selector); 
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function deepestChild() {
+  // this is interesting?
+  return document.querySelector('#grand-node div div div div');
+}
+
+function inccreaseRankBy(n) {
+  
+}
